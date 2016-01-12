dfafd8330b3b7095e5379970e065b0add17e8bc3
Razvan Pascalau <mail@razvan-pascalau.com>
2016-01-13 00:44:44 +0200

    initial commit

---
+++ SVGRasterizer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SVGRasterizer
+//
+//  Created by Razvan Pascalau on 13/01/16.
+//  Copyright © 2016 Razvan Pascalau. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

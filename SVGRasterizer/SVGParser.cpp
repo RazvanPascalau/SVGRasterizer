12861e50d7dbdf99eeaf39de9915e477406f439d
Razvan Pascalau <mail@razvan-pascalau.com>
2016-01-16 00:38:31 +0200

    removed pugs as submodule

--- SVGRasterizer/SVGParser.cpp
+++ SVGRasterizer/SVGParser.cpp
@@ -16,6 +16,10 @@ bool parseSVG(const std::string& filePath)
 
     pugi::xml_parse_result result = doc.load_file(filePath.c_str());
 
+    if (result.status != pugi::status_ok)
+        return false;
+    std::cout << "First node name: [" << doc.first_child().name() << "]\n";
+
     //    std::cout << "Load result: " << result.description() << ", mesh name: " << doc.child("mesh").attribute("name").value() << std::endl;
     return false;
 }
